5e94453bb2bf9175a81e0527b89c8a701bf300bf
Bastien <bastien.baret@gmail.com>
2020-11-02 23:07:17 +0100

    heroku

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import 'normalize.css/normalize.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
-import '@blueprintjs/datetime/lib/css/blueprint-datetime.css'
 import { Provider } from 'next-auth/client'
 
 function MyApp({ Component, pageProps }) {
