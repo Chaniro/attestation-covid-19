85094243b962842bfae97cb6d92eaa0b063b67c8
Bastien <bastien.baret@gmail.com>
2020-11-03 20:16:50 +0100

    Update reasons

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,16 @@ import 'normalize.css/normalize.css'
 import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 import '@blueprintjs/core/lib/css/blueprint.css'
 import { Provider } from 'next-auth/client'
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
   return (
       <Provider session={pageProps.session}>
+          <Head>
+              <title>Attestation</title>
+              <meta name="theme-color" content="#30404d" />
+          </Head>
         <Component {...pageProps} />
       </Provider>
   );
