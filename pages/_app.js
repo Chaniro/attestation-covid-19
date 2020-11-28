8d944877ba89a6b5c17b3c357166930976988edf
Bastien <bastien.baret@gmail.com>
2020-11-28 10:18:01 +0100

    Update to latest document

--- pages/_app.js
+++ pages/_app.js
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }) {
               <title>Attestation COVID-19</title>
 
               <meta charSet='utf-8'/>
-              <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+              <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
               <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
 
               <link rel="manifest" href="/manifest.json"/>
